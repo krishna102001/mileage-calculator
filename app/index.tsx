6a7218ae6045972e2c7d09e041cd29046bcba2e6
krishna102001 <krishnakantmaurya@icloud.com>
2025-08-12 11:48:39 +0530

    setup tailwind css

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "./global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-amber-600'>
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
