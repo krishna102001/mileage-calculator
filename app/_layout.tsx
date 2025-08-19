1219c345a3423f53919e71167ff55e7f0ed47642
krishna102001 <krishnakantmaurya@icloud.com>
2025-08-19 17:22:17 +0530

    fixed some issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { drizzle } from "drizzle-orm/expo-sqlite";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import { Stack } from "expo-router";
 import { SQLiteProvider, useSQLiteContext } from "expo-sqlite";
+import { useEffect } from "react";
 import { StatusBar } from "react-native";
 
 export const DATABASE_NAME = "milehistory";
@@ -10,9 +11,16 @@ export const DATABASE_NAME = "milehistory";
 function Database_Initializer() {
   const db = drizzle(useSQLiteContext());
   const { success, error } = useMigrations(db, migrations);
-  if (error) {
-    console.log("migration error :", error);
-  }
+
+  useEffect(() => {
+    if (error) {
+      console.error("Migration error:", error);
+    }
+    if (success) {
+      console.log("Migrations completed successfully");
+    }
+  }, [success, error]);
+
   return (
     <Stack>
       <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
